784a5c44f63465409ee7b14a21b9714e150b27e1
Steve Gordon <sgordon@hotmail.co.uk>
2019-07-04 13:25:55 +0100

    Renaming exception

---
+++ src/QueueReadingChannelsSample/Sqs/AmazonSQSException.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace QueueReadingChannelsSample.Sqs
+{
+    public class AmazonSqsException : Exception
+    {
+        public AmazonSqsException() : base("The security token included in the request is expired")
+        {
+        }
+    }
+}
